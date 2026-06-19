b9f8d150e7bfe0471f7ca057d1a8b2b8f65e5758
Nguyễn Minh Duy <duyxyz.git@gmail.com>
2026-06-20 00:30:33 +0700

    Add files via upload

---
+++ Enable.Developer.Tools.on.Discord.cmd
@@ -0,0 +1,16 @@
+(
+echo {
+echo   "IS_MAXIMIZED": true,
+echo   "IS_MINIMIZED": false,
+echo   "WINDOW_BOUNDS": {
+echo     "x": 112,
+echo     "y": 60,
+echo     "width": 1284,
+echo     "height": 724
+echo   },
+echo   "DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING": true,
+echo   "MIN_WIDTH": 940,
+echo   "MIN_HEIGHT": 500,
+echo   "chromiumSwitches": {}
+echo }
+) > "C:\Users\PC\AppData\Roaming\discord\settings.json"
